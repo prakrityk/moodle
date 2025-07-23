21fc48c4fad6ceeb8f9f4db8b4d244f3afadadd6
Prakrity Khanal <prakrity.khanal021@apexcollege.edu.np>
2025-07-23 22:31:21 +0545

    child theme customization

--- theme/prakrity/lib.php
+++ theme/prakrity/lib.php
@@ -6,3 +6,28 @@
 defined('MOODLE_INTERNAL') || die();
 
 // We will add callbacks here as we add features to our theme.
+
+function theme_prakrity_get_main_scss_content($theme) {
+    global $CFG;
+
+    $scss = '';
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+    $fs = get_file_storage();
+
+    $context = context_system::instance();
+    if ($filename == 'default.scss') {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    } else if ($filename == 'plain.scss') {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_prakrity', 'preset', 0, '/', $filename))) {
+        $scss .= $presetfile->get_content();
+    } else {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    }
+
+    // Add your pre.scss and post.scss
+    $pre = file_get_contents($CFG->dirroot . '/theme/prakrity/scss/pre.scss');
+    $post = file_get_contents($CFG->dirroot . '/theme/prakrity/scss/post.scss');
+
+    return $pre . "\n" . $scss . "\n" . $post;
+}
